9684a5353358c06178ee37427f6b5f7cf52ace7a
kariaki58 <programmerjavascript901@gmail.com>
2024-03-07 13:36:20 +0100

    changed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,48 +1,13 @@
-#!/usr/bin/python3
-"""
-draw pascal triangle
-"""
-
-
-def factoral(n):
-    """find factorial of a number"""
-    if n == -1:
-        return 0
-    if n == 0:
-        return 1
-    return n * factoral(n - 1)
-
-
-def find_number(n, m):
-    """find the number at the nth row and mth column"""
-    n_num = n - 1
-    m_num = m - 1
-
-    n_com_fac = factoral(n_num)
-    m_com_fac = factoral(m_num)
-    m_com_other_fac = factoral(m)
-
-    hope_not_zero = (m_com_fac * factoral(n_num - m_num))
-
-    if n == 0 and m == 0:
-        return 1
-    if hope_not_zero:
-        main_comb_num = int(n_com_fac / hope_not_zero)
-    else:
-        main_comb_num = 0
-    if n_num - m < 0:
-        m_com_other_fac_num = 0
-    else:
-        cal = int(n_com_fac / (m_com_other_fac * factoral(n_num - m)))
-        m_com_other_fac_num = cal
-    return m_com_other_fac_num + main_comb_num
-
-
 def pascal_triangle(n):
-    """return the diagram"""
-    triangle = []
-    if type(n) is not int or n <= 0:
-        return triangle
-    for i in range(n):
-        triangle.append([find_number(i, j) for j in range(i + 1)])
-    return (triangle)
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
