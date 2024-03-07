43653e748c739e200666139f3d86d9addb6bb5ca
kariaki58 <programmerjavascript901@gmail.com>
2024-03-07 13:41:27 +0100

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-'''A module for working with Pascal's triangle.
-'''
+"""solving pascal triangle"""
 
 
 def pascal_triangle(n):
-    '''Creates a list of lists of integers representing
-    the Pascal's triangle of a given integer.
-    '''
+    """pascal triangle"""
     triangle = []
-    if type(n) is not int or n <= 0:
+    if n <= 0:
         return triangle
     for i in range(n):
-        line = []
+        inner_numbers = []
         for j in range(i + 1):
             if j == 0 or j == i:
-                line.append(1)
+                inner_numbers.append(1)
             elif i > 0 and j > 0:
-                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        triangle.append(line)
+                inner_numbers.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(inner_numbers)
     return triangle
---
+++ 0x00-pascal_triangle/pascal.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+draw pascal triangle
+"""
+
+
+def factoral(n):
+    """find factorial of a number"""
+    if n == -1:
+        return 0
+    if n == 0:
+        return 1
+    return n * factoral(n - 1)
+
+
+def find_number(n, m):
+    """find the number at the nth row and mth column"""
+    n_num = n - 1
+    m_num = m - 1
+
+    n_com_fac = factoral(n_num)
+    m_com_fac = factoral(m_num)
+    m_com_other_fac = factoral(m)
+
+    hope_not_zero = (m_com_fac * factoral(n_num - m_num))
+
+    if n == 0 and m == 0:
+        return 1
+    if hope_not_zero:
+        main_comb_num = int(n_com_fac / hope_not_zero)
+    else:
+        main_comb_num = 0
+    if n_num - m < 0:
+        m_com_other_fac_num = 0
+    else:
+        cal = int(n_com_fac / (m_com_other_fac * factoral(n_num - m)))
+        m_com_other_fac_num = cal
+    return m_com_other_fac_num + main_comb_num
+
+
+def pascal_triangle(n):
+    """return the diagram"""
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        triangle.append([find_number(i, j) for j in range(i + 1)])
+    return (triangle)
