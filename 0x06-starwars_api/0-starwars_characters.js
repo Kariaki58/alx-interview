55caadc2c7b2398f084ab2b9ca8d67a2adfb1136
kariaki58 <programmerjavascript901@gmail.com>
2024-04-18 15:52:28 +0100

    starwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv.slice(2);
+
+
+const resource = 'https://swapi-api.alx-tools.com/api/films/' + args[0];
+
+request(resource, async function (error, response, body) {
+  if (!error) {
+    const json = JSON.parse(body);
+    const enpoint = json.characters;
+    for (const data of enpoint) {
+      await new Promise(function (resolve, reject) {
+        request(data, function (error, response, body) {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
+      });
+    }
+  }
+});
