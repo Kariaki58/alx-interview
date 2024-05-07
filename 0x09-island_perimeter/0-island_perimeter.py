36acb8dab4fcd5b331ba14d88266a8ec5e37f755
Kariaki58 <kariakistephen809@gmail.com>
2024-05-07 12:30:34 +0100

    added python path and documentation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+"""island perimeter"""
+
 def island_perimeter(grid):
+    """island function"""
     perimeter = 0
     rows, cols = len(grid), len(grid[0])
-
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
@@ -10,5 +13,4 @@ def island_perimeter(grid):
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
-
     return perimeter
