74e2e001f3103d7e9fef9237d1e5b60f5d567d44
Kariaki58 <kariakistephen809@gmail.com>
2024-05-14 11:24:30 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,43 +1,28 @@
 #!/usr/bin/python3
-"""prime game graph theory"""
+"""Prime Game"""
 
-def isPrime(n):
-    """is prime"""
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-def generatePrimes(n):
-    """generate primes"""
-    primes = []
-    for i in range(2, n + 1):
-        if isPrime(i):
-            primes.append(i)
-    return primes
 
 def isWinner(x, nums):
-    """isWinner"""
+    """is winner"""
     maria_wins = 0
     ben_wins = 0
-    for n in nums:
-        primes = generatePrimes(n)
-        if len(primes) % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
+    if x < 1 or not nums:
+        return None 
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for idx1, is_prime in enumerate(primes, 1):
+        if idx1 == 1 or not is_prime:
+            continue
+        for idx2 in range(idx1 + idx1, n + 1, idx1):
+            primes[idx2 - 1] = False
+    for _, n in zip(range(x), nums):
+        p_count = len(list(filter(lambda y: y, primes[0: n])))
+        ben_wins += p_count % 2 == 0
+        maria_wins += p_count % 2 == 1
+    if maria_wins == ben_wins:
+        return None
     if maria_wins > ben_wins:
         return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
     else:
-        return None
-
+        return "Ben"
