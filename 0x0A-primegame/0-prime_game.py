fdc7375714ecc090f3f3cfd0898bb87033e98ff0
Kariaki58 <kariakistephen809@gmail.com>
2024-05-14 11:32:19 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """Prime Game"""
 
+def Prime(n):
+    """isPrime"""
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    return primes
 
 def isWinner(x, nums):
     """is winner"""
-    maria_wins = 0
-    ben_wins = 0
+    player1 = 0
+    player2 = 0
     if x < 1 or not nums:
         return None 
     n = max(nums)
-    primes = [True for _ in range(1, n + 1, 1)]
-    primes[0] = False
+    primes = Prime(n)
     for idx1, is_prime in enumerate(primes, 1):
         if idx1 == 1 or not is_prime:
             continue
@@ -18,11 +22,11 @@ def isWinner(x, nums):
             primes[idx2 - 1] = False
     for _, n in zip(range(x), nums):
         p_count = len(list(filter(lambda y: y, primes[0: n])))
-        ben_wins += p_count % 2 == 0
-        maria_wins += p_count % 2 == 1
-    if maria_wins == ben_wins:
+        player2 += p_count % 2 == 0
+        player1 += p_count % 2 == 1
+    if player1 == player2:
         return None
-    if maria_wins > ben_wins:
+    if player1 > player2:
         return "Maria"
     else:
         return "Ben"
